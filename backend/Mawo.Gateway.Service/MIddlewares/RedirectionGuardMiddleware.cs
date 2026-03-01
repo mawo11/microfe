c16f1b499d8386c9a5521419a1f35cf2882ab4f7
mawo11 <mawo11@gmail.com>
2026-03-01 21:30:40 +0100

    poprawki

--- backend/Mawo.Gateway.Service/MIddlewares/RedirectionGuardMiddleware.cs
+++ backend/Mawo.Gateway.Service/MIddlewares/RedirectionGuardMiddleware.cs
@@ -4,13 +4,25 @@ public sealed partial class RedirectionGuardMiddleware : IMiddleware
 {
 	public async Task InvokeAsync(HttpContext context, RequestDelegate next)
 	{
+		context.Response.OnStarting(() =>
+		{
+			if (context.Response.Headers.TryGetValue("Location", out var location))
+			{
+				try
+				{
+					var uri = new Uri(location.ToString());
+					context.Response.Headers["Location"] = uri.PathAndQuery;
+				}
+				catch
+				{
+					// ignoruj jeśli Location nie jest pełnym URI
+				}
+			}
+
+			return Task.CompletedTask;
+		});
+
 		await next(context);
 
-		if (context.Response.Headers.TryGetValue("Location", out var location))
-		{
-			var uri = new Uri(location.ToString());
-			// zostaw tylko ścieżkę
-			context.Response.Headers["Location"] = uri.PathAndQuery;
-		}
 	}
 }
